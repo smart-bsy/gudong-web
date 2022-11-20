3c93807057d412b8a069595cced84dbbb46936c9
root <root@fdsa.localdomain>
2022-11-21 03:01:24 +0800

    5

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 1s",
+      },
+      keyframes: {
+        fadeIn: {
+          from: {
+            opacity: 0,
+            transform: "translate3d(0, 100%,0)",
+          },
+          to: {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+      },
+      dropShadow: {
+        "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+        "4xl": [
+          "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "0 45px 65px rgba(0, 0, 0, 0.15)",
+        ],
+      },
+    },
   },
   plugins: [],
 };
